19b660b7fbec882a5ade15d1affa07acf3df04dd
David Himmelstrup <lemmih@gmail.com>
2023-03-20 18:20:55 +0100

    silence zip

--- daily_snapshot_terraform/modules/daily_snapshot/prep_sources.sh
+++ daily_snapshot_terraform/modules/daily_snapshot/prep_sources.sh
@@ -5,6 +5,6 @@
 cd "$1" || exit
 cp -r ../../../scripts/ruby_common service/
 
-(cd service && zip -r -X ../sources.zip .)
+(cd service && zip -r -X ../sources.zip . > /dev/null 2>&1)
 rm -fr service/ruby_common
 echo "{ \"path\": \"$1/sources.zip\" }"
