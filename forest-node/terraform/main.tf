e118664b88e4d13e68b2706fc22e562a1caff37b
samuelarogbonlo <sbayo971@gmail.com>
2023-03-22 16:12:23 +0100

    Changed Output positions for the droplet IPs (#2423)

--- forest-node/terraform/main.tf
+++ forest-node/terraform/main.tf
@@ -52,8 +52,3 @@ ${digitalocean_droplet.forest.ipv4_address}
 ${digitalocean_droplet.forest_observability.ipv4_address}
     _EOF
 }
-
-output "ip" {
-  description = "output of created droplets"
-  value = [digitalocean_droplet.forest.ipv4_address, digitalocean_droplet.forest_observability.ipv4_address]
-}
---
+++ forest-node/terraform/outputs.tf
@@ -0,0 +1,4 @@
+output "ip" {
+  description = "output of created droplets"
+  value = [digitalocean_droplet.forest.ipv4_address, digitalocean_droplet.forest_observability.ipv4_address]
+}
