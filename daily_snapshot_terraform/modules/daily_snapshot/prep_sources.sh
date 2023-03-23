8c6e61e7fd7bf907cb3210beddf631c3f94dbb1c
David Himmelstrup <lemmih@gmail.com>
2023-03-23 15:44:40 +0100

    fix tar file, simplify init script

--- daily_snapshot_terraform/modules/daily_snapshot/prep_sources.sh
+++ daily_snapshot_terraform/modules/daily_snapshot/prep_sources.sh
@@ -3,8 +3,9 @@
 # Copy local source files in a folder together with ruby_common and create a zip archive.
 
 cd "$1" || exit
-cp -r ../../../scripts/ruby_common service/
+cp --archive ../../../scripts/ruby_common service/
 
-(cd service && zip -r -X ../sources.zip . > /dev/null 2>&1)
+rm -f sources.tar
+(cd service && tar cf ../sources.tar * --sort=name --mtime='UTC 2019-01-01' > /dev/null 2>&1)
 rm -fr service/ruby_common
-echo "{ \"path\": \"$1/sources.zip\" }"
+echo "{ \"path\": \"$1/sources.tar\" }"
