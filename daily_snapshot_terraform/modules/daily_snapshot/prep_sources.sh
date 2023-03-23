d38171652bd6aff75bc5c02f2173ca52331534f0
David Himmelstrup <lemmih@gmail.com>
2023-03-23 15:51:16 +0100

    apply shellcheck lint

--- daily_snapshot_terraform/modules/daily_snapshot/prep_sources.sh
+++ daily_snapshot_terraform/modules/daily_snapshot/prep_sources.sh
@@ -6,6 +6,6 @@ cd "$1" || exit
 cp --archive ../../../scripts/ruby_common service/
 
 rm -f sources.tar
-(cd service && tar cf ../sources.tar * --sort=name --mtime='UTC 2019-01-01' > /dev/null 2>&1)
+(cd service && tar cf ../sources.tar ./* --sort=name --mtime='UTC 2019-01-01' > /dev/null 2>&1)
 rm -fr service/ruby_common
 echo "{ \"path\": \"$1/sources.tar\" }"
