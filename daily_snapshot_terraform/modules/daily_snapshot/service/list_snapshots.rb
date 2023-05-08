7078a24a545db5f89e0b1513f296b8992e5aa938
David Himmelstrup <lemmih@gmail.com>
2023-05-08 14:38:43 +0200

    purge compressed snapshots if they exist

--- daily_snapshot_terraform/modules/daily_snapshot/service/list_snapshots.rb
+++ daily_snapshot_terraform/modules/daily_snapshot/service/list_snapshots.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 require 'active_support/time'
 
-Snapshot = Struct.new(:network, :date, :height, :compressed, :file_name, :url_s3, :url) do
+Snapshot = Struct.new(:network, :date, :height, :file_name, :url_s3, :url) do
   def delete
     system("s3cmd del #{url_s3}")
     checksum = Pathname.new(url_s3).sub_ext('.sha256sum')
     system("s3cmd del #{checksum}")
+    compressed = Pathname.new(url_s3).sub_ext('.car.zst')
+    system("s3cmd del #{compressed}")
   end
 end
 
 # List the snapshots available in the S3 space hosted by Forest
 def list_snapshots(chain_name = 'calibnet', bucket = 'forest-snapshots', endpoint = 'fra1.digitaloceanspaces.com')
   ls_format = /\d{4}-\d{2}-\d{2} \d{2}:\d{2}\s*\d*\s*s3:\/\/#{bucket}\/#{chain_name}\/(.+)/
-  snapshot_format = /^([^_]+?)_snapshot_(?<network>[^_]+?)_(?<date>\d{4}-\d{2}-\d{2})_height_(?<height>\d+)\.car(?<compressed>\.zst)?$/
+  snapshot_format = /^([^_]+?)_snapshot_(?<network>[^_]+?)_(?<date>\d{4}-\d{2}-\d{2})_height_(?<height>\d+)\.car$/
 
   output = `s3cmd ls s3://#{bucket}/#{chain_name}/`
 
@@ -24,7 +26,7 @@ def list_snapshots(chain_name = 'calibnet', bucket = 'forest-snapshots', endpoin
       file.match(snapshot_format) { |m| 
         url = "https://#{bucket}.#{endpoint}/#{chain_name}/#{file}"
         url_s3 = "s3://#{bucket}/#{chain_name}/#{file}"
-        snapshot = Snapshot.new m[:network], m[:date].to_date, m[:height].to_i, m[:compressed], file, url_s3, url
+        snapshot = Snapshot.new m[:network], m[:date].to_date, m[:height].to_i, file, url_s3, url
         snapshot_list << snapshot
       }
     }
