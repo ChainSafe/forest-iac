5c00f05f380ad445a2f159538483c08c0681d930
David Himmelstrup <lemmih@gmail.com>
2023-05-08 10:12:38 +0200

    feat: use volumes for snapshot generation (#71)

--- daily_snapshot_terraform/modules/daily_snapshot/service/snapshots_prune.rb
+++ daily_snapshot_terraform/modules/daily_snapshot/service/snapshots_prune.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'list_snapshots'
+
 require 'date'
 require 'pathname'
 
@@ -22,47 +24,45 @@ end
 # Represents Day Bucket. They key is the date.
 class DayBucket < SnapshotBucket
   def add?(entry)
-    super File.mtime(entry).to_date
+    super entry.date
   end
 end
 
 # Represents Weeks Bucket. The key is "WWYY" (week starts on Monday).
 class WeeksBucket < SnapshotBucket
   def add?(entry)
-    super File.mtime(entry).to_date.strftime('%m%y')
+    super entry.date.strftime('%m%y')
   end
 end
 
 # Represents Months Bucket. The key is "MMYY"
 class MonthsBucket < SnapshotBucket
   def add?(entry)
-    super File.mtime(entry).to_date.strftime('%m%y')
+    super entry.date.strftime('%m%y')
   end
 end
 
 # Prunes snapshots directory with the following retention policy:
+# * keep at most 1 snapshot per day
 # * keep all snapshots generated in the last 7 days,
 # * keep one snapshot per week for the last 4 weeks,
 # * keep one snapshot per month after 4 weeks.
 #
 # Returns pruned snapshots' filenames.
-def prune_snapshots(snapshots_directory)
+def prune_snapshots(snapshots)
+  day_unique_bucket = DayBucket.new
   day_bucket = DayBucket.new 7
   weeks_bucket = WeeksBucket.new 4
   months_bucket = MonthsBucket.new
   buckets = [day_bucket, weeks_bucket, months_bucket]
 
   # iterate over each entry and try to add it to the buckets, newest first.
-  Dir.glob(File.join(snapshots_directory, '*.car'))
-     .sort_by { |f| File.mtime(f) }
-     .reverse
-     .reject  { |f| buckets.any? { |bucket| bucket.add? f } }
-     .each    { |f| remove_snapshot f }
-end
-
-# Removes the snapshot and optionally the related checksum file if it exists.
-def remove_snapshot(snapshot)
-  checksum = Pathname.new(snapshot).sub_ext('.sha256sum')
-  File.delete checksum if checksum.file?
-  File.delete snapshot
+  snapshots
+    # ignore the first snapshot to keep a buffer of two recent snapshots.
+    # this makes it less likely that we delete a snapshot that is being downloaded.
+    .drop(1)
+    # keep snapshots (ie reject) if they fit in a bucket while also having a unique date
+    .reject  { |f| day_unique_bucket.add? f and buckets.any? { |bucket| bucket.add? f } }
+    # delete all snapshots that weren't rejected or dropped
+    .each(&:delete)
 end
