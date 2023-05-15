fd62ee21e8527d2b8ddc9da4848ced3e3c562b5d
Hubert Bugaj <hubert@chainsafe.io>
2023-05-15 12:36:26 +0200

    make Snapshot a class

--- daily_snapshot_terraform/modules/daily_snapshot/service/list_snapshots.rb
+++ daily_snapshot_terraform/modules/daily_snapshot/service/list_snapshots.rb
@@ -1,12 +1,43 @@
 # frozen_string_literal: true
+
 require 'active_support/time'
 
-Snapshot = Struct.new(:network, :date, :height, :file_name, :url_s3, :url) do
+# Filecoin snapshot class
+# - network: mainnet, calibnet
+# - date: date when the snapshot was taken
+# - height: height of the snapshot (epochs)
+# - file_name: name of the file
+# - url_s3: url of the snapshot on S3
+# - url: url of the snapshot on the web
+class Snapshot
+  attr_accessor :network, :date, :height, :file_name, :url_s3, :url
+
+  def initialize(network, date, height, file_name, url_s3, url)
+    if network.nil? || date.nil? || height.nil? || file_name.nil? || url_s3.nil? || url.nil?
+      raise ArgumentError, 'Missing argument'
+    end
+
+    network = network.downcase
+
+    if network != 'mainnet' && network != 'calibnet'
+      raise ArgumentError, 'Invalid network'
+    end
+
+    @network = network
+    @date = date.to_date
+    @height = height.to_i
+    @file_name = file_name
+    @url_s3 = url_s3
+    @url = url
+  end
+
+  # Deletes the snapshot from S3 - the full snapshot,
+  # the checksum and the compressed version.
   def delete
-    system("s3cmd del #{url_s3}")
-    checksum = Pathname.new(url_s3).sub_ext('.sha256sum')
+    system("s3cmd del #{@url_s3}")
+    checksum = Pathname.new(@url_s3).sub_ext('.sha256sum')
     system("s3cmd del #{checksum}")
-    compressed = Pathname.new(url_s3).sub_ext('.car.zst')
+    compressed = Pathname.new(@url_s3).sub_ext('.car.zst')
     system("s3cmd del #{compressed}")
   end
 end
@@ -21,15 +52,15 @@ def list_snapshots(chain_name = 'calibnet', bucket = 'forest-snapshots', endpoin
   snapshot_list = []
 
   output.each_line do |line|
-    line.match(ls_format) { |m|
+    line.match(ls_format) do |m|
       file = m.captures[0]
-      file.match(snapshot_format) { |m| 
+      file.match(snapshot_format) do |m|
         url = "https://#{bucket}.#{endpoint}/#{chain_name}/#{file}"
         url_s3 = "s3://#{bucket}/#{chain_name}/#{file}"
-        snapshot = Snapshot.new m[:network], m[:date].to_date, m[:height].to_i, file, url_s3, url
+        snapshot = Snapshot.new m[:network], m[:date], m[:height], file, url_s3, url
         snapshot_list << snapshot
-      }
-    }
+      end
+    end
   end
   # Sort the snapshots by decreasing height
   snapshot_list.sort_by { |a| -a.height }
