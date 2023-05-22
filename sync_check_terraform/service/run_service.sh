7cd9d690ea48dcb0474e64a9d3f88b95bdf302c4
sudo-shashank <shashank@chainsafe.io>
2023-05-22 10:59:07 +0530

    remove watchtower

--- sync_check_terraform/service/run_service.sh
+++ sync_check_terraform/service/run_service.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-## Ensure watchtower is running
-docker stop watchtower 2> /dev/null || true
-docker wait watchtower 2> /dev/null || true
-docker run --rm \
-    --detach \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    --name watchtower \
-    containrrr/watchtower \
-    --label-enable --include-stopped --revive-stopped --stop-timeout 120s --interval 600
-
 # Clean volumes
 rm -rf /var/lib/docker/volumes/forest-data/_data/*
 rm -rf /var/lib/docker/volumes/sync-check/_data/*
