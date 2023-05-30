f1d0d46b855c8ca80c3005ec7c179b019b6c130f
samuelarogbonlo <sbayo971@gmail.com>
2023-05-30 19:01:55 +0100

    Added feature to allow mainnet and calibnet deploy based on volume and chain logic(#58)

--- terraform/modules/forest_node/user-data.sh
+++ terraform/modules/forest_node/user-data.sh
@@ -45,27 +45,24 @@ if [ -n "${VOLUME_NAME}" ]; then
   # Change ownership of volume directory to the created user
   chown -R "${NEW_USER}":"${NEW_USER}" "/home/${NEW_USER}/forest_data"
 
-  # Make sure /etc/${NEW_USER} config directory exists and set the required permmission
-  mkdir --parents -- /etc/"${NEW_USER}" && chmod 777 /etc/"${NEW_USER}"
-
   # Create the config.toml file
-  cat << EOF > /etc/"${NEW_USER}"/config.toml
+  cat << EOF > /home/"${NEW_USER}"/forest_data/config.toml
 [client]
-data_dir = "/home/${NEW_USER}/data"
+data_dir = "/home/${NEW_USER}/forest_data/data"
 EOF
 
   su - "${NEW_USER}" -c "docker run -d --name forest \
-    -v /etc/${NEW_USER}/config.toml:/home/${NEW_USER}/config.toml \
     -v /home/${NEW_USER}/forest_data:/home/${NEW_USER}/data \
     -p 1234:1234 \
     --restart always \
     ghcr.io/chainsafe/forest:latest \
-    --config /home/${NEW_USER}/config.toml \
+    --config /home/${NEW_USER}/data/config.toml \
     --encrypt-keystore false \
     --auto-download-snapshot \
     --chain ${CHAIN}"
 
 else
+
   # If a volume name was not provided, run the Forest Mainnet or Calibnet Docker container without a volume.
   su - "${NEW_USER}" -c "docker run -d --name forest \
     -p 1234:1234 \
