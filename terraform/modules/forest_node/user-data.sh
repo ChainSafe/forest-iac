b6dea8fd6ab2e31928f7b692003c88fcf6a0f427
samuelarogbonlo <sbayo971@gmail.com>
2023-05-31 13:49:17 +0100

    Revamped the script to commodate best practices(#58)

--- terraform/modules/forest_node/user-data.sh
+++ terraform/modules/forest_node/user-data.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-
 set -euxo pipefail
 
 # Create a login user
-useradd --create-home -- "${NEW_USER}"
+adduser --home "/home/${NEW_USER}" --disabled-password --gecos "" "${NEW_USER}"
 
 # Create .ssh directory for created user
 mkdir --parents -- "/home/${NEW_USER}/.ssh"
@@ -25,45 +24,41 @@ echo "AllowUsers ${NEW_USER}" >> /etc/ssh/sshd_config
 systemctl restart sshd
 
 # Add user to sudoers file. We pipe to visudo to check syntax
-echo "${NEW_USER} ALL=(ALL) NOPASSWD: ALL" | (EDITOR="tee --append" visudo)
+usermod --append --groups sudo "${NEW_USER}"
 
 # Add new user to "docker" group
 usermod --append --groups docker "${NEW_USER}"
 
+# set-up forest data directory for Forest container
+mkdir --parents -- "/home/${NEW_USER}/forest_data"
 
 # Check if the volume name is defined
-# If the volume name is defined, start the Forest Docker container with volume
+# If the volume name is defined, mount the volume
 if [ -n "${VOLUME_NAME}" ]; then
 
-  # set-up forest volume for Forest container
-  mkdir --parents -- "/home/${NEW_USER}/forest_data"
-
+  # mount volume at the forest data directory
   # discard: notify the volume to free blocks (useful for SSDs)
   # defaults: default mount options, including rw
   # noatime: don't preserve file access times
   mount --options discard,defaults,noatime /dev/disk/by-id/scsi-0DO_Volume_"${DISK_ID_VOLUME_NAME}" "/home/${NEW_USER}/forest_data"
+fi
 
-  # Change ownership of volume directory to the created user
-  chown -R "${NEW_USER}":"${NEW_USER}" "/home/${NEW_USER}/forest_data"
+# Change ownership of directory to the created user
+chown -R "${NEW_USER}":"${NEW_USER}" "/home/${NEW_USER}/forest_data"
 
-  # Create the config.toml file
-  cat << EOF > /home/"${NEW_USER}"/forest_data/config.toml
+# Create the config.toml file
+cat << EOF > "/home/forest/forest_data/config.toml"
 [client]
 data_dir = "/home/${NEW_USER}/forest_data/data"
 EOF
 
-  VOLUME_OPTION="-v /home/${NEW_USER}/forest_data:/home/${NEW_USER}/data"
-  CONFIG_OPTION="--config /home/${NEW_USER}/data/config.toml"
-
-fi
-
 # Run the Forest Docker container
 su - "${NEW_USER}" -c "docker run -d --name forest \
-  $${VOLUME_OPTION:+$VOLUME_OPTION} \
+  -v /home/${NEW_USER}/forest_data:/home/${NEW_USER}/data \
   -p 1234:1234 \
   --restart always \
   ghcr.io/chainsafe/forest:latest \
+  --config /home/${NEW_USER}/data/config.toml \
   --encrypt-keystore false \
   --auto-download-snapshot \
-  --chain ${CHAIN} \
-  $${CONFIG_OPTION:+$CONFIG_OPTION}"
+  --chain ${CHAIN}"
