bf637b1a5057b9e1d090b49877cec7e38904059b
samuelarogbonlo <sbayo971@gmail.com>
2023-06-01 12:46:40 +0100

    Reverted some changes in the boot service script(#58)

--- scripts/sync_check/boot_service.sh
+++ scripts/sync_check/boot_service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# The .env file contains the chain variables that we want access to.
+# The .env file contains environment variables that we want access to.
 set -o allexport
 # Trust that the `.env` exists in the CWD during the script execution.
 # shellcheck disable=SC1091
@@ -9,7 +9,7 @@ set +o allexport
 
 error=0
 
-# Check if a chain variable is set. If it isn't, set error=1.
+# Check if an environment variable is set. If it isn't, set error=1.
 check_env () {
     A="                            ";
     echo -n "${A:0:-${#1}} $1: "
@@ -21,7 +21,7 @@ check_env () {
     fi
 }
 
-# Check that the chain variables in the .env file have been defined.
+# Check that the environment variables in the .env file have been defined.
 check_env "FOREST_SLACK_API_TOKEN"
 check_env "FOREST_SLACK_NOTIF_CHANNEL"
 check_env "FOREST_TAG"
@@ -30,7 +30,7 @@ check_env "FOREST_TARGET_DATA"
 check_env "FOREST_TARGET_RUBY_COMMON"
 
 if [ "$error" -ne "0" ]; then
-    echo "Please set the required chain variables and try again."
+    echo "Please set the required environment variables and try again."
     exit 1
 fi
 
