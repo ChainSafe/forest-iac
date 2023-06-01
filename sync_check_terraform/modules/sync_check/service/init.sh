0684a395500424c91e0005bc43ef516a897e00ef
sudo-shashank <shashank@chainsafe.io>
2023-06-01 19:56:18 +0530

    simplify init script

--- sync_check_terraform/modules/sync_check/service/init.sh
+++ sync_check_terraform/modules/sync_check/service/init.sh
@@ -5,11 +5,8 @@ dnf install -y dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 dnf install -y docker-ce docker-ce-cli containerd.io
 dnf install -y docker-buildx-plugin docker-compose-plugin docker-compose
-dnf install -y ruby
-dnf install -y ruby-devel
-dnf install -y gcc make
+dnf install -y ruby ruby-devel gcc make
 dnf clean all
-gem install slack-ruby-client
-gem install sys-filesystem
+gem install slack-ruby-client sys-filesystem
 
 nohup /bin/bash ./run_service.sh > run_service_log.txt &
