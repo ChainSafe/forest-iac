ee023027870d7d9b10f12469e151dd595fb3acd7
sudo-shashank <shashank@chainsafe.io>
2023-06-05 19:02:51 +0530

    fix shellcheck

--- sync_check_terraform/modules/sync_check/prep_sources.sh
+++ sync_check_terraform/modules/sync_check/prep_sources.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Enable strict error handling, command tracing, and pipefail
-set -euxo pipefail
+# Enable strict error handling and command tracing
+set -eux
 
 # Copy local source files in a folder together with ruby_common and create a zip archive.
 
