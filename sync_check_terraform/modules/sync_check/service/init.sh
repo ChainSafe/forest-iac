46b4265b006570cf79e23bfa87a6ccb9d8aea189
sudo-shashank <shashank@chainsafe.io>
2023-06-05 16:53:42 +0530

    Simplify init and fix fmt

--- sync_check_terraform/modules/sync_check/service/init.sh
+++ sync_check_terraform/modules/sync_check/service/init.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -euxo pipefail
 
 ## Install dependencies
-dnf install -y dnf-plugins-core
-dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-dnf install -y docker-ce docker-ce-cli containerd.io
-dnf install -y docker-buildx-plugin docker-compose-plugin docker-compose
-dnf install -y ruby ruby-devel gcc make
+dnf install -y dnf-plugins-core && \
+dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo && \
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose ruby ruby-devel gcc make && \
 dnf clean all
 gem install slack-ruby-client sys-filesystem
 
