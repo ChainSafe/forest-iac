a3bd3dc2d1df9cdf669c32a2617d717da6798606
sudo-shashank <shashank@chainsafe.io>
2023-06-14 12:04:39 +0530

    Added benchmark db terraform service

---
+++ benchmark_db_terraform/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  backend "s3" {
+    # Note: This is the bucket for the internal terraform state. This bucket is
+    # completely independent from the bucket that contains snapshots.
+    bucket = "forest-iac"
+    # This key uniquely identifies the service. To create a new service (instead
+    # of modifying this one), use a new key. Unfortunately, variables may not be
+    # used here.
+    key = "benchmark_db.tfstate"
+
+    # This value is completely unused by DO but _must_ be a known AWS region.
+    region = "us-west-1"
+    # The S3 region is determined by the endpoint. fra1 = Frankfurt.
+    # This region does not have to be shared by the droplet.
+    endpoint = "https://fra1.digitaloceanspaces.com"
+
+    # Credentially can be validated through the Security Token Service (STS).
+    # Unfortunately, DigitalOcean does not support STS so we have to skip the
+    # validation.
+    skip_credentials_validation = "true"
+  }
+}
+
+module "benchmark_db" {
+  # Import the benchmark db module
+  source = "./modules/benchmark_db"
+
+  # Configure service:
+  name          = "forest-benchmark" # droplet name
+  size          = "so-2vcpu-16gb"    # droplet size
+  slack_channel = "#forest-dump"     # slack channel for notifications
+
+  # Variable passthrough:
+  slack_token           = var.slack_token
+  AWS_ACCESS_KEY_ID     = var.AWS_ACCESS_KEY_ID
+  AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
+  digitalocean_token    = var.do_token
+}
+
+# This ip address may be used in the future by monitoring software
+output "ip" {
+  value = [module.benchmark_db.ip]
+}
---
+++ benchmark_db_terraform/modules/benchmark_db/variable.tf
@@ -0,0 +1,82 @@
+variable "digitalocean_token" {
+  description = "Token for authentication."
+  type        = string
+}
+
+variable "name" {
+  description = "The name of Forest Droplet"
+  type        = string
+}
+
+variable "size" {
+  description = "The size of the droplet instance to launch"
+  type        = string
+}
+
+variable "slack_channel" {
+  description = "slack channel name for notifications"
+  type        = string
+}
+
+variable "slack_token" {
+  description = "slack access token"
+  type        = string
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "S3 access key id"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "S3 private access key"
+  type        = string
+}
+
+variable "snapshot_bucket" {
+  description = "S3 bucket containing the snapshots"
+  type        = string
+  default     = "forest-snapshots"
+}
+
+variable "snapshot_endpoint" {
+  description = "S3 endpoint for the snapshots"
+  type        = string
+  default     = "https://fra1.digitaloceanspaces.com/"
+}
+
+variable "forest_tag" {
+  description = "Image tag for the Forest container"
+  type        = string
+  default     = "latest"
+}
+
+variable "image" {
+  description = "The ID of the AMI to use for the Droplet"
+  type        = string
+  default     = "fedora-36-x64"
+}
+
+variable "region" {
+  description = "The region where resources will be created"
+  type        = string
+  default     = "fra1"
+}
+
+variable "project" {
+  description = "DigitalOcean project used as parent for the created droplet"
+  type        = string
+  default     = "Forest-DEV" # Alternative: "Default"
+}
+
+variable "source_addresses" {
+  description = "List of source addresses."
+  type        = list(string)
+  default     = ["0.0.0.0/0", "::/0"]
+}
+
+variable "destination_addresses" {
+  description = "List of destination addresses."
+  type        = list(string)
+  default     = ["0.0.0.0/0", "::/0"]
+}
