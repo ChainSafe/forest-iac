41b8ab30bf1945b790a7b61a85544a914933b717
samuelarogbonlo <sbayo971@gmail.com>
2023-06-22 12:52:28 +0100

    added more comments to main.tf
    Signed-off-by: samuelarogbonlo <sbayo971@gmail.com>

--- new-relic/main.tf
+++ new-relic/main.tf
@@ -1,3 +1,6 @@
+# This Terraform script configures an environment to use New Relic for infrastructure monitoring
+# and alerting, including setting up alert policies and a notification channel for Slack.
+
 terraform {
   required_version = "~> 1.0"
   required_providers {
@@ -22,11 +25,32 @@ provider "newrelic" {
   region     = "EU"                  # Valid regions are US and EU
 }
 
+# This block of code uses Terraform's data source to fetch details of an existing New Relic 
+# alert policy named "Golden Signals". The "Golden Signals" are a set of monitoring parameters
+# that originate from the Google SRE (Site Reliability Engineering) Handbook. They provide
+# a high level overview of a system's health and are typically included in most monitoring setups.
+#
+# In the context of New Relic, the "Golden Signals" alert policy is created by default 
+#  when a new New Relic account is created. This policy includes a set of predefined alert conditions based
+# on the Google's Golden Signals concept.
+#
+# By fetching this policy using the data source, we can integrate these conditions with other
+# resources managed in this script, such as linking it with a notification channel or adding it 
+# to a workflow.
 
+data "newrelic_alert_policy" "golden_signals" {
+  name = "Golden Signals"
+}
+
+# Creation of a new New Relic alert policy for infrastructure or Contianer downtime 
 resource "newrelic_alert_policy" "alert" {
   name = "Infrastruture Downtime Alert"
 }
 
+# Different NRQL alert conditions for various types of events including host down,
+# high disk space, high RAM utilization, high memory utilization, and container issues
+# Each condition has specific criteria and triggers for warning and critical alerts
+# A NRQL (New Relic Query Language) query is used to determine when the alert conditions are met
 resource "newrelic_nrql_alert_condition" "host_down" {
   policy_id                    = newrelic_alert_policy.alert.id
   type                         = "static"
@@ -58,7 +82,6 @@ resource "newrelic_nrql_alert_condition" "disk_space" {
   type                         = "static"
   name                         = "disk_space"
   description                  = "Alert when disk space usage is high on any host"
-  runbook_url                  = "https://www.example.com"
   enabled                      = true
   violation_time_limit_seconds = 3600
 
@@ -155,34 +178,42 @@ resource "newrelic_nrql_alert_condition" "container_issue" {
   }
 }
 
-# Notification channel
-# resource "newrelic_notification_channel" "slack_notification_channel" {
-#   name           = "Sample Notification Channel"
-#   type           = "EMAIL"
-#   destination_id = newrelic_notification_destination.sample_notification_destination.id
-#   product        = "IINT"
+# Setting up a Slack channel as the notification channel for alerts
+resource "newrelic_notification_channel" "slack-channel" {
+  name = "slack"
+  type = "SLACK"
+  destination_id = var.slack_destination_id
+  product = "IINT"
 
-#   property {
-#     key   = "subject"
-#     value = "Sample Email Subject"
-#   }
-# }
+  property {
+    key = "channelId"
+    value = var.slack_channel_id
+  }  
+  property {
+    key = "customDetailsSlack"
+    value = "The '{{  annotations.description }}' has been activated. The condition has exceeded the defined threshold. Kindly examine this issue on the New Relic dashboard for more extensive data and potential mitigation steps."
 
-# resource "newrelic_workflow" "slack_workflow" {
-#   name                  = "Sample Workflow"
-#   muting_rules_handling = "NOTIFY_ALL_ISSUES"
+  }
+}
 
-#   issues_filter {
-#     name = "Issue Filter"
-#     type = "FILTER"
-#     predicate {
-#       attribute = "labels.policyIds"
-#       operator  = "EXACTLY_MATCHES"
-#       values    = [newrelic_alert_policy.alert_policy_name.id]
-#     }
-#   }
+# Creation of a New Relic workflow that includes issues filtered by the policy IDs
+# and sends notifications to the configured Slack channel
+resource "newrelic_workflow" "slack_workflow" {
+  name = "Slack Workflow"
+  muting_rules_handling = "NOTIFY_ALL_ISSUES"
 
-#   destination {
-#     channel_id = newrelic_notification_channel.sample_notification_channel.id
-#   }
-# }
+  issues_filter {
+    name = "Filter-name"
+    type = "FILTER"
+
+    predicate {
+      attribute = "labels.policyIds"
+      operator = "EXACTLY_MATCHES"
+      values = [newrelic_alert_policy.alert.id, data.newrelic_alert_policy.golden_signals.id] 
+    }
+  }
+
+  destination {
+    channel_id = newrelic_notification_channel.slack-channel.id
+  }
+}
--- new-relic/variable.tf
+++ new-relic/variable.tf
@@ -7,3 +7,14 @@ variable "NEW_RELIC_API_KEY" {
   description = "The New Relic API KEY"
   type        = string
 }
+
+variable "slack_destination_id" {
+  description = "The unique identifier for the Slack workspace where notifications will be sent."
+  type        = string
+}
+
+variable "slack_channel_id" {
+  description = "The unique identifier for the Slack channel where notifications will be posted."
+  type        = string
+}
+
