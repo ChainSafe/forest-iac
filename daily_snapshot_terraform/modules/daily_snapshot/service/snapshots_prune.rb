75ab4922ff891ddb642e27702eb21821bc0e00ef
Hubert Bugaj <hubert@chainsafe.io>
2023-06-27 15:47:30 +0200

    relax pruning rules plus add rspec tests

--- daily_snapshot_terraform/modules/daily_snapshot/service/snapshots_prune.rb
+++ daily_snapshot_terraform/modules/daily_snapshot/service/snapshots_prune.rb
@@ -31,30 +31,21 @@ end
 # Represents Weeks Bucket. The key is "WWYY" (week starts on Monday).
 class WeeksBucket < SnapshotBucket
   def add?(entry)
-    super entry.date.strftime('%m%y')
-  end
-end
-
-# Represents Months Bucket. The key is "MMYY"
-class MonthsBucket < SnapshotBucket
-  def add?(entry)
-    super entry.date.strftime('%m%y')
+    super entry.date.strftime('%W%y')
   end
 end
 
 # Prunes snapshots directory with the following retention policy:
-# * keep at most 1 snapshot per day
+# * keep at most 2 snapshots per day
 # * keep all snapshots generated in the last 7 days,
-# * keep one snapshot per week for the last 4 weeks,
-# * keep one snapshot per month after 4 weeks.
+# * keep one snapshot per week afterwards.
 #
 # Returns pruned snapshots' filenames.
 def prune_snapshots(snapshots)
   day_unique_bucket = DayBucket.new
   day_bucket = DayBucket.new 7
-  weeks_bucket = WeeksBucket.new 4
-  months_bucket = MonthsBucket.new
-  buckets = [day_bucket, weeks_bucket, months_bucket]
+  weeks_bucket = WeeksBucket.new
+  buckets = [day_bucket, weeks_bucket]
 
   # iterate over each entry and try to add it to the buckets, newest first.
   snapshots
