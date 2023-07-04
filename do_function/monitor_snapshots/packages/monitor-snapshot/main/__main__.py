2350d805b27c15c29722c1a2016b9b9cad206529
samuelarogbonlo <sbayo971@gmail.com>
2023-07-04 19:56:16 +0100

    improved the alert message and added trigger for the function
    Signed-off-by: samuelarogbonlo <sbayo971@gmail.com>

--- do_function/monitor_snapshots/packages/monitor-snapshot/main/__main__.py
+++ do_function/monitor_snapshots/packages/monitor-snapshot/main/__main__.py
@@ -1,9 +1,11 @@
 import requests
 from xml.etree import ElementTree as ET
 import re
+import os
 from datetime import datetime, timezone, timedelta
 from slack import WebClient
 from slack.errors import SlackApiError
+import json
 
 # DigitalOcean Spaces details
 region = "fra1"
@@ -16,9 +18,12 @@ pattern = r'([^_]+?)_snapshot_([^_]+?)_(\d{4}-\d{2}-\d{2})_height_(\d+).car(.zst
 # Specify the folders to check
 folders = ["mainnet", "calibnet"]
 
-def slack_alert(message):
+def slack_alert(message_dict):
     client = WebClient(token=os.environ['SLACK_TOKEN'])
     CHANNEL_ID = 'C05BHMZ7GTT'
+    
+    # Create formatted message as string resembling JSON
+    message = f'```{json.dumps(message_dict, indent=4, ensure_ascii=False)}```'
     
     try:
         response = client.chat_postMessage(channel=CHANNEL_ID, text=message)
@@ -33,6 +38,7 @@ def get_snapshots():
     for child in root:
         snapshot_dict = {}
         snapshot_name = ''
+        # Extracting snapshot details (name, size, and last modified time)
         for snapshot in child:
             if snapshot.tag.endswith('Key'):
                 snapshot_name = snapshot.text
@@ -40,7 +46,8 @@ def get_snapshots():
                 snapshot_dict['Size'] = int(snapshot.text)
             elif snapshot.tag.endswith('LastModified'):
                 snapshot_dict['LastModified'] = datetime.strptime(snapshot.text, '%Y-%m-%dT%H:%M:%S.%fZ')
-
+        
+        # Adding snapshot details to dictionary if file type is valid
         if snapshot_name.endswith(('.car', '.car.zst', '.sha256sum')):
             folder_name = snapshot_name.split('/')[0]
             if folder_name not in snapshots:
@@ -48,6 +55,14 @@ def get_snapshots():
             snapshots[folder_name][snapshot_name] = snapshot_dict
     return snapshots
 
+# Main function to perform checks on the snapshots
+# For each folder, perform series of checks:
+    # - Check if snapshots exist
+    # - Verify snapshot age
+    # - Confirm snapshot size is > 1GB
+    # - Validate snapshot name
+    # - Ensure full snapshots have corresponding .sha256sum file
+    # - Detect stray .sha256sum files
 def main():
     all_snapshots = get_snapshots()
     checks_passed = True
@@ -75,44 +90,40 @@ def main():
 
         if snapshot_age_hours > 30:
             checks_passed = False
-            print(f"⛔ The latest {folder} snapshot: {base_url}/{latest_snapshot_filename}{base_url}/{latest_snapshot_filename} is older than 30 hours (Age: {snapshot_age_hours:.2f} hours). 🔥🌲🔥")
+            slack_alert(f"⛔ The latest {folder} snapshot: {base_url}/{latest_snapshot_filename} is older than 30 hours (Age: {snapshot_age_hours:.2f} hours). 🔥🌲🔥")
         else:
-            print(f"✅ The latest {folder} snapshot: {base_url}/{latest_snapshot_filename}{base_url}/{latest_snapshot_filename} is not older than 30 hours (Age: {snapshot_age_hours:.2f} hours). 🌲🌳🌲🌳🌲")
-
+            slack_alert(f"✅ The latest {folder} snapshot: {base_url}/{latest_snapshot_filename} is not older than 30 hours (Age: {snapshot_age_hours:.2f} hours. 🌲🌳🌲🌳🌲")
+            
         for snapshot_name, snapshot in snapshots.items():
             if snapshot['Size'] < 1073741824 and snapshot_name.endswith(('.car', '.car.zst')):  # 1GB in bytes 
                 checks_passed = False
-                print(f"Snapshot {snapshot_name} is smaller than 1GB")
+                slack_alert(f"🚫 Error! The snapshot {snapshot_name} is less than 1GB. Size anomalies detected. 📉")
 
             # Check if the snapshot name matches the pattern
             if not re.match(pattern, latest_snapshot_name) and snapshot_name.endswith(('.car', '.car.zst')):
-                print(f"Snapshot {snapshot_name} does not match the name pattern")
+                slack_alert(f"🚫 Error! The snapshot {snapshot_name} does not conform to the standard naming convention. Please check. 📝")
 
             # Check if full snapshots have a corresponding shasum file
             if snapshot_name.endswith(('.car', '.car.zst')):  # assuming this is a full snapshot
                 shasum_file = snapshot_name.rsplit('.', 1)[0] + '.sha256sum'
                 if shasum_file not in snapshots:
                     checks_passed = False
-                    print(f"Full snapshot {snapshot_name} does not have a corresponding shasum file")
+                    slack_alert(f"⚠️ Warning! The full snapshot {snapshot_name} is missing its corresponding .sha256sum file. Check required. 🔍")
             elif snapshot_name.endswith('.sha256sum'):  # check for stray shasum files
                 snapshot_file = snapshot_name.rsplit('.', 1)[0] + '.car'
                 snapshot_file_zst = snapshot_name.rsplit('.', 1)[0] + '.car.zst'
                 if snapshot_file not in snapshots and snapshot_file_zst not in snapshots:
                     checks_passed = False
-                    print(f"Stray shasum file {snapshot_name} found")
+                    print(f"🚨 Error! Stray .sha256sum file {snapshot_name} detected. Please verify. 🕵️")
 
     if checks_passed:
-        return {"result": "✅ success", 
-                "message": "All checks passed successfully. All snapshots are valid and up-to-date. Lets keep up the good work! 🌲🌳🌲🌳🌲"}
+        message = {"result": "✅ success", 
+                   "message": "All checks passed successfully. All snapshots are valid and up-to-date. Lets keep up the good work! 🌲🌳🌲🌳🌲"}
+        slack_alert(message)
+        return message
     else:
-        return {"result": "⛔ failure", 
-                "message": "Some checks did not pass. Please review the issues reported above. Let's fix them and keep the forest green! 🔥🌲🔥"}
-                
-
-if __name__ == "__main__":
-    main()
-
-
-
-#slack_alert(f"✅ The latest {folder} snapshot is not older than 30 hours. 🌲🌳🌲🌳🌲")
-#slack_alert(f"Latest {folder} snapshot link: {base_url}/{latest_snapshot_filename}")
+        message = {"result": "⛔ failure", 
+                   "message": "Some checks did not pass. Please review the issues reported above. Let's fix them and keep the forest green! 🔥🌲🔥"}
+        slack_alert(message)
+        return message
+    
