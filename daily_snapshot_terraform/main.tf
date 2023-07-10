805b5f0cf183c69165a232534dae84c6f9fa5bb1
samuelarogbonlo <sbayo971@gmail.com>
2023-07-10 10:18:15 +0100

    fixed terraform fmt errors
    Signed-off-by: samuelarogbonlo <sbayo971@gmail.com>

--- daily_snapshot_terraform/main.tf
+++ daily_snapshot_terraform/main.tf
@@ -28,7 +28,7 @@ module "daily_snapshot" {
   source = "./modules/daily_snapshot"
 
   # Configure service:
-  name              = "forest-snapshot"   # droplet name
+  name              = "forest-snapshot"       # droplet name
   size              = "so-2vcpu-16gb"         # droplet size
   slack_channel     = "#forest-notifications" # slack channel for notifications
   snapshot_bucket   = "forest-snapshots"
