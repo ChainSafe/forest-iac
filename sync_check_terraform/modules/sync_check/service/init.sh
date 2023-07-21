248fd558763fd303dc708feca8e78359424e15d7
Hubert <hubert@chainsafe.io>
2023-07-22 01:06:43 +0200

    use standard fedora docker (#169)

--- sync_check_terraform/modules/sync_check/service/init.sh
+++ sync_check_terraform/modules/sync_check/service/init.sh
@@ -4,9 +4,8 @@
 set -euxo pipefail
 
 ## Install dependencies
-dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo && \
-dnf install -y dnf-plugins-core docker-ce docker-ce-cli containerd.io docker-buildx-plugin ruby ruby-devel gcc make && \
-dnf clean all
+dnf install -y dnf-plugins-core docker docker-compose ruby ruby-devel gcc make && \
+  dnf clean all
 gem install slack-ruby-client sys-filesystem
 
 nohup /bin/bash ./run_service.sh > run_service_log.txt &
