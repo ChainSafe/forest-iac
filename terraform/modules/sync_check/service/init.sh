fd5b80c71e4b45bfd256f532114b40a40d8c7a70
samuelarogbonlo <sbayo971@gmail.com>
2023-08-08 09:15:27 +0100

    convert sync from fedora to ubuntu
    Signed-off-by: samuelarogbonlo <sbayo971@gmail.com>

--- terraform/modules/sync_check/service/init.sh
+++ terraform/modules/sync_check/service/init.sh
@@ -4,7 +4,7 @@
 set -euxo pipefail
 
 ## Install dependencies
-apt-get install -y docker docker-compose ruby ruby-dev gcc make && \
+apt-get install -y docker-compose ruby ruby-dev gcc make && \
   apt clean
 gem install slack-ruby-client sys-filesystem
 
