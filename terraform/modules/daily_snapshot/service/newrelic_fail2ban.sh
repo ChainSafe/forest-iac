6fd4c10e24fed7d9b014abb18cb555101d033460
samuelarogbonlo <sbayo971@gmail.com>
2023-08-10 15:29:25 +0100

    fixed failling ci
    Signed-off-by: samuelarogbonlo <sbayo971@gmail.com>

--- terraform/modules/daily_snapshot/service/newrelic_fail2ban.sh
+++ terraform/modules/daily_snapshot/service/newrelic_fail2ban.sh
@@ -5,7 +5,6 @@
 # It also installs Fail2Ban, sets up its default configuration, and enables it to start at boot
 
 set -euo pipefail
-
 # If new relic API key is provided, install the new relic agent
 if [ -n "$NEW_RELIC_API_KEY" ] ; then
   curl -Ls https://download.newrelic.com/install/newrelic-cli/scripts/install.sh | bash && \
@@ -37,3 +36,4 @@ fi
 sudo apt-get install fail2ban -y
 sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 sudo systemctl enable fail2ban && sudo systemctl start fail2ban
+
