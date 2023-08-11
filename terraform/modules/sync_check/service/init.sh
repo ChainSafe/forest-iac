9330fdc4d59ec996ea725ed323514fb41ce72436
samuelarogbonlo <sbayo971@gmail.com>
2023-08-11 09:25:55 +0100

    revert sync check changes
    Signed-off-by: samuelarogbonlo <sbayo971@gmail.com>

--- terraform/modules/sync_check/service/init.sh
+++ terraform/modules/sync_check/service/init.sh
@@ -4,7 +4,8 @@
 set -euxo pipefail
 
 ## Install dependencies
-sudo apt-get install -y ruby ruby-dev gcc make
+dnf install -y dnf-plugins-core docker docker-compose ruby ruby-devel gcc make && \
+  dnf clean all
 gem install slack-ruby-client sys-filesystem
 
 nohup /bin/bash ./run_service.sh > run_service_log.txt &
