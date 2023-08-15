d2b58eb789c0af4e81bf7847cd905cffa441cdab
samuelarogbonlo <sbayo971@gmail.com>
2023-08-15 08:44:19 +0100

    added more strict config to new relic
    Signed-off-by: samuelarogbonlo <sbayo971@gmail.com>

--- terraform/modules/sync_check/service/init.sh
+++ terraform/modules/sync_check/service/init.sh
@@ -16,6 +16,8 @@ if [ -n "$NEW_RELIC_API_KEY" ] ; then
   NEW_RELIC_REGION="$NEW_RELIC_REGION" \
   /usr/local/bin/newrelic install -y
 
+# The provided configurations are specific to New Relic. To gain a deeper understanding of these configuration details, you can visit:
+# https://docs.newrelic.com/docs/infrastructure/install-infrastructure-agent/configuration/infrastructure-agent-configuration-settings/#offline-time-to-reset
 cat >> /etc/newrelic-infra.yml <<EOF
 include_matching_metrics:
   process.name:
@@ -25,9 +27,11 @@ include_matching_metrics:
     - regex "^syslog.*"
     - regex "^gpg-agent.*"
 metrics_network_sample_rate: -1
-metrics_process_sample_rate: 300
+metrics_process_sample_rate:-1
 metrics_system_sample_rate: 300
 metrics_storage_sample_rate: 300
+metrics_nfs_sample_rate: 300
+container_cache_metadata_limit: 300
 disable_zero_mem_process_filter: true
 disable_all_plugins: true
 disable_cloud_metadata: true 
