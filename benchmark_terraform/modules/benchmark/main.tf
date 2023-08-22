9437db16f7c948415124bcb5b0bc8ee1e6c14b2b
samuelarogbonlo <sbayo971@gmail.com>
2023-08-22 12:21:09 +0100

    fixed failling lint
    Signed-off-by: samuelarogbonlo <sbayo971@gmail.com>

--- benchmark_terraform/modules/benchmark/main.tf
+++ benchmark_terraform/modules/benchmark/main.tf
@@ -13,6 +13,15 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.1"
+    }
+
   }
 }
 
