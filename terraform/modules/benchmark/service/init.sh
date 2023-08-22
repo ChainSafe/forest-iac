44de259f8a7778216f1afc90327b199264ccd594
sudo-shashank <shashank@chainsafe.io>
2023-08-22 20:15:37 +0530

    fix container issues

--- terraform/modules/benchmark/service/init.sh
+++ terraform/modules/benchmark/service/init.sh
@@ -6,10 +6,10 @@ set -eux
 # Check if Docker is installed
 if ! command -v docker &> /dev/null; then
     echo "Docker not found. Installing Docker..."
-    sudo apt-get install -y docker
+    sudo apt-get install -y docker docker-compose
     sudo systemctl enable docker
     sudo systemctl start docker
 fi
 
 # Run Docker Compose using the provided file which runs the benchmark
-docker compose -f docker-compose.yml up -d --force-recreate
+docker-compose -f docker-compose.yml up -d --force-recreate
