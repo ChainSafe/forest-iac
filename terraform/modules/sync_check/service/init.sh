8d43edeb0bc0ea6631200b2506acf6329c074009
samuelarogbonlo <sbayo971@gmail.com>
2023-08-23 11:56:34 +0100

    update comments
    Signed-off-by: samuelarogbonlo <sbayo971@gmail.com>

--- terraform/modules/sync_check/service/init.sh
+++ terraform/modules/sync_check/service/init.sh
@@ -4,7 +4,7 @@
 set -eux
 
 
-# Use APT specific mechanism to ensure non-interactive operation and wait indefinitely for the lock
+# Use APT specific mechanism to ensure non-interactive operation and wait for the lock
 sudo DEBIAN_FRONTEND=noninteractive apt-get -qqq --yes -o DPkg::Lock::Timeout=-1 update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -qqq --yes -o DPkg::Lock::Timeout=-1 install -y ruby ruby-dev s3cmd anacron
 
