e0a5bf85c4b05fa8d86ab350dba47fa779c2d156
sudo-shashank <shashank@chainsafe.io>
2023-08-23 14:04:48 +0530

    Remove docker installation

--- terraform/modules/benchmark/service/init.sh
+++ terraform/modules/benchmark/service/init.sh
@@ -3,14 +3,6 @@
 ## Enable strict error handling
 set -eux 
 
-# Check if Docker is installed
-if ! command -v docker &> /dev/null; then
-    echo "Docker not found. Installing Docker..."
-    sudo apt-get install -y docker
-    sudo systemctl enable docker
-    sudo systemctl start docker
-fi
-
 sudo docker build -t benchmark .
 
 echo "Starting benchmark docker service.."
