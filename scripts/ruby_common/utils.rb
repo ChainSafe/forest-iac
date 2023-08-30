543bd10e362490fcd50a130366cd12b39382852a
Josh Jones <joshua.jones@chainsafe.io>
2023-08-30 17:05:27 +0100

    style: rubocop

--- scripts/ruby_common/utils.rb
+++ scripts/ruby_common/utils.rb
@@ -2,7 +2,7 @@
 
 # Retrieves an environmental variable, failing if its not set or empty.
 def get_and_assert_env_variable(name)
-  var = ENV[name]
+  var = ENV(name, nil)
   raise "Please set #{name} environmental variable" if var.nil? || var.empty?
 
   var
