39e82db2b8127eeb5eb98364134cfc7a7a44a6a7
David Himmelstrup <david.himmelstrup@chainsafe.io>
2023-09-13 11:58:31 +0200

    quick and dirty file listing

--- cf/forest-snapshot-listing/src/worker.ts
+++ cf/forest-snapshot-listing/src/worker.ts
@@ -8,20 +8,81 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-export default {
-  async fetch(request, env, ctx) {
-    const listing = await env.FOREST_ARCHIVE.list();
-    let html = `<!DOCTYPE html>
+const BASE_URL = "https://forest-archive.chainsafe.dev";
+
+async function do_listing(env, prefix) {
+  const options = {
+    limit: 500,
+    prefix: prefix,
+  };
+
+  const listed = await env.FOREST_ARCHIVE.list(options);
+  let truncated = listed.truncated;
+  let cursor = truncated ? listed.cursor : undefined;
+
+  // use the truncated property to check if there are more
+  // objects to be returned
+  while (truncated) {
+    const next = await env.FOREST_ARCHIVE.list({
+      ...options,
+      cursor: cursor,
+    });
+    listed.objects.push(...next.objects);
+
+    truncated = next.truncated;
+    cursor = next.cursor
+  }
+
+
+  let html = `<!DOCTYPE html>
 		<body>
 		  <h1>Forest Archive</h1>
-      Files: ${listing.objects.length}`;
-    html += `</body>`;
+      <ul>`;
+  for (let obj of listed.objects) {
+    html += `<li><a href="${BASE_URL}/${obj.key}">${obj.key}</a></li>\n`;
+  }
+  html += `</ul></body>`;
 
-    return new Response(html, {
-      headers: {
-        "content-type": "text/html;charset=UTF-8",
-      },
-    });
-    return new Response('Hello World!');
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html;charset=UTF-8",
+    },
+  });
+}
+
+export default {
+  async fetch(request, env, ctx) {
+
+    const url = new URL(request.url);
+    const { pathname, search } = url;
+
+    switch (pathname) {
+      case '/list/':
+        const html = `<!DOCTYPE html>
+		      <body>
+		      <h1>Forest Archive</h1>
+          <ul>
+          <li><a href="/list/calibnet/diff">Calibnet Diffs</a></li>
+          <li><a href="/list/calibnet/lite">Calibnet Lite</a></li>
+          <li><a href="/list/mainnet/diff">Mainnet Diffs</a></li>
+          <li><a href="/list/mainnet/lite">Mainnet Lite</a></li>
+          </ul>
+          </body>`;
+        return new Response(html, {
+          headers: {
+            "content-type": "text/html;charset=UTF-8",
+          },
+        });
+      case '/list/mainnet/diff':
+        return do_listing(env, 'mainnet/diff');
+      case '/list/calibnet/diff':
+        return do_listing(env, 'calibnet/diff');
+      case '/list/mainnet/lite':
+        return do_listing(env, 'mainnet/lite');
+      case '/list/calibnet/lite':
+        return do_listing(env, 'calibnet/lite');
+      default:
+        return new Response(`url: ${pathname}`);
+    }
   },
 };
