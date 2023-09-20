fa3fa2e57d86dfc6fa8a52084f53e21819fee749
Hubert Bugaj <hubert@chainsafe.io>
2023-09-20 15:34:25 +0200

    enable typescript linters

--- cf/latest-snapshot/src/index.ts
+++ cf/latest-snapshot/src/index.ts
@@ -1,17 +1,17 @@
 import parseRange from 'range-parser';
 
 interface Env {
-	FOREST_ARCHIVE: R2Bucket,
+	FOREST_ARCHIVE: R2Bucket;
 }
 
-function basename(path: String) {
+function basename(path: string) {
 	return path.split('/').reverse()[0];
 }
 
 // Directly fetch the data for the latest snapshot of a given chain (eg. calibnet or mainnet)
 async function get_latest(req_headers: Headers, env: Env, chain: string): Promise<Response> {
-	const listed = await env.FOREST_ARCHIVE.list({ prefix: chain + "/latest/" });
-	let latest = listed.objects.at(-1);
+	const listed = await env.FOREST_ARCHIVE.list({ prefix: chain + '/latest/' });
+	const latest = listed.objects.at(-1);
 	if (latest == null) {
 		return new Response(`No latest snapshot found ${chain}`, {
 			status: 404,
@@ -34,7 +34,7 @@ async function get_latest(req_headers: Headers, env: Env, chain: string): Promis
 		// The `parseRange` returns an error code if the range is not satisfiable (or not specified),
 		// so we can handle the range request only if it returns an array.
 		let status = 200;
-		let range = parseRange(object.size, req_headers.get("range") || "");
+		const range = parseRange(object.size, req_headers.get('range') || '');
 		if (Array.isArray(range)) {
 			// R2Object doesn't support multiple ranges, so we only use the first one.
 			// Throw an error if there are more than one range.
@@ -44,8 +44,8 @@ async function get_latest(req_headers: Headers, env: Env, chain: string): Promis
 				});
 			}
 
-			let r = range[0];
-			headers.set("Content-Range", `${r.type} ${r.start}-${r.end}/${object.size}`);
+			const r = range[0];
+			headers.set('Content-Range', `${r.type} ${r.start}-${r.end}/${object.size}`);
 			status = 206; // Partial Content
 		}
 
@@ -53,17 +53,20 @@ async function get_latest(req_headers: Headers, env: Env, chain: string): Promis
 		headers.set('etag', object.httpEtag);
 		const encoded_name = encodeURIComponent(basename(object.key));
 		// Tell browsers and aria2c which filename to use. For 'wget', you have to use `--trust-server-names`.
-		headers.set('Content-Disposition', `attachment; filename*=UTF-8''${encoded_name}; filename="${encoded_name}"`);
+		headers.set(
+			'Content-Disposition',
+			`attachment; filename*=UTF-8''${encoded_name}; filename="${encoded_name}"`
+		);
 
 		if ('body' in object) {
 			return new Response(object.body, {
 				headers,
-				status
+				status,
 			});
 		} else {
 			return new Response(null, {
 				headers,
-				status
+				status,
 			});
 		}
 	}
@@ -72,7 +75,7 @@ async function get_latest(req_headers: Headers, env: Env, chain: string): Promis
 export default {
 	async fetch(request: Request, env: Env): Promise<Response> {
 		const url = new URL(request.url);
-		const chain = (url.pathname.match(/\/latest\/(\w*)/) || ["undefined"])[1];
+		const chain = (url.pathname.match(/\/latest\/(\w*)/) || ['undefined'])[1];
 
 		// Disallow any other request method except GET, they are not sensible in the context
 		// of fetching a snapshot.
@@ -80,7 +83,7 @@ export default {
 			return new Response('Method not allowed', {
 				status: 405,
 				headers: {
-					'Allow': 'GET',
+					Allow: 'GET',
 				},
 			});
 		}
