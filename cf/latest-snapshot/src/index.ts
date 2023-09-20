1146449374e4539e2daf73a0170cb0a9449690cb
Hubert Bugaj <hubert@chainsafe.io>
2023-09-20 11:35:10 +0200

    support range queries

--- cf/latest-snapshot/src/index.ts
+++ cf/latest-snapshot/src/index.ts
@@ -1,3 +1,5 @@
+import parseRange from 'range-parser';
+
 interface Env {
 	FOREST_ARCHIVE: R2Bucket,
 }
@@ -7,7 +9,7 @@ function basename(path: String) {
 }
 
 // Directly fetch the data for the latest snapshot of a given chain (eg. calibnet or mainnet)
-async function get_latest(env: Env, chain: string): Promise<Response> {
+async function get_latest(req_headers: Headers, env: Env, chain: string): Promise<Response> {
 	const listed = await env.FOREST_ARCHIVE.list({ prefix: chain + "/latest/" });
 	let latest = listed.objects.at(-1);
 	if (latest == null) {
@@ -15,23 +17,48 @@ async function get_latest(env: Env, chain: string): Promise<Response> {
 			status: 404,
 		});
 	} else {
-		// Should we support range queries?
-		const object = await env.FOREST_ARCHIVE.get(latest.key);
+		const object = await env.FOREST_ARCHIVE.get(latest.key, {
+			range: req_headers,
+			onlyIf: req_headers,
+		});
+
 		if (object === null) {
 			return new Response('No latest snapshot found', {
 				status: 404,
 			});
 		}
+
 		const headers = new Headers();
+
+		// If the client requested a range, then we need to set the Content-Range header.
+		// The `parseRange` returns an error code if the range is not satisfiable (or not specified),
+		// so we can handle the range request only if it returns an array.
+		let status = 200;
+		let range = parseRange(object.size, req_headers.get("range") || "");
+		if (Array.isArray(range)) {
+			status = 206; // Partial Content
+			range.forEach(function (r) {
+				headers.append("Content-Range", `${range.type} ${r.start}-${r.end}/${object.size}`);
+			})
+		}
+
 		object.writeHttpMetadata(headers);
 		headers.set('etag', object.httpEtag);
-		let encoded_name = encodeURIComponent(basename(object.key));
+		const encoded_name = encodeURIComponent(basename(object.key));
 		// Tell browsers and aria2c which filename to use. For 'wget', you have to use `--trust-server-names`.
-		headers.set('content-disposition', `attachment; filename*=UTF-8''${encoded_name}; filename="${encoded_name}"`);
+		headers.set('Content-Disposition', `attachment; filename*=UTF-8''${encoded_name}; filename="${encoded_name}"`);
 
-		return new Response(object.body, {
-			headers,
-		});
+		if ('body' in object) {
+			return new Response(object.body, {
+				headers,
+				status
+			});
+		} else {
+			return new Response(null, {
+				headers,
+				status
+			});
+		}
 	}
 }
 
@@ -40,6 +67,16 @@ export default {
 		const url = new URL(request.url);
 		const chain = (url.pathname.match(/\/latest\/(\w*)/) || ["undefined"])[1];
 
-		return await get_latest(env, chain);
+		// Disallow any other request method except GET, they are not sensible in the context
+		// of fetching a snapshot.
+		if (request.method !== 'GET') {
+			return new Response('Method not allowed', {
+				status: 405,
+				headers: {
+					'Allow': 'GET',
+				},
+			});
+		}
+		return await get_latest(request.headers, env, chain);
 	},
 };
