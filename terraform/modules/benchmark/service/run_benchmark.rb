449dd234114db4709daa880ff80958414bd602a4
samuelarogbonlo <sbayo971@gmail.com>
2023-09-22 06:36:58 +0100

    fix ci
    Signed-off-by: samuelarogbonlo <sbayo971@gmail.com>

--- terraform/modules/benchmark/service/run_benchmark.rb
+++ terraform/modules/benchmark/service/run_benchmark.rb
@@ -16,7 +16,6 @@ def get_and_assert_env_variable(name)
   var
 end
 
-
 def file_last_modified_date
   file_url = 'https://forest-benchmarks.fra1.digitaloceanspaces.com/benchmark-results/all-results.csv'
   response = Net::HTTP.get_response(URI(file_url))
