d4950e9f337a56d977365643eb10d52e69aada7b
David Himmelstrup <david.himmelstrup@chainsafe.io>
2023-10-06 10:41:18 +0200

    fix: wait for cloud-init to finish (#303)

--- terraform/modules/daily_snapshot/service/init.sh
+++ terraform/modules/daily_snapshot/service/init.sh
@@ -2,12 +2,15 @@
 
 set -eux
 
+# Wait for cloud-init to finish initializing the machine
+cloud-init status --wait
+
 # Setting DEBIAN_FRONTEND to ensure non-interactive operations for APT
 export DEBIAN_FRONTEND=noninteractive
 
 # Use APT specific mechanism to wait for the lock
-apt-get -qqq --yes -o DPkg::Lock::Timeout=90 update
-apt-get -qqq --yes -o DPkg::Lock::Timeout=90 install -y ruby ruby-dev anacron awscli
+apt-get -qqq --yes update
+apt-get -qqq --yes install -y ruby ruby-dev anacron awscli
 
 # Install the gems
 gem install docker-api slack-ruby-client
