a7f76a46997aab9b255a25c916648b55b86e52e3
samuelarogbonlo <sbayo971@gmail.com>
2023-10-11 14:47:24 +0100

    chore: use terraform latest version
    Signed-off-by: samuelarogbonlo <sbayo971@gmail.com>

--- terraform/new-relic/main.tf
+++ terraform/new-relic/main.tf
@@ -10,12 +10,15 @@ terraform {
     }
   }
   backend "s3" {
-    bucket                      = "forest-iac"
-    key                         = "new_relic/terraform.tfstate"
-    region                      = "us-west-1"
-    endpoint                    = "fra1.digitaloceanspaces.com"
+    bucket = "forest-iac"
+    key    = "new_relic/terraform.tfstate"
+    region = "us-west-1"
+    endpoints = {
+      s3 = "fra1.digitaloceanspaces.com"
+    }
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
   }
 }
 
