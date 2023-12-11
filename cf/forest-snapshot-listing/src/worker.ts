7359822a4e1106f2fd7c38f6c679ab423f78916f
lemmih <lemmih@gmail.com>
2023-12-11 11:18:07 +0100

    use spaces rather than tabs

--- cf/forest-snapshot-listing/src/worker.ts
+++ cf/forest-snapshot-listing/src/worker.ts
@@ -1,59 +1,59 @@
 const BASE_URL = 'https://forest-archive.chainsafe.dev';
 
 interface Env {
-	FOREST_ARCHIVE: R2Bucket;
+  FOREST_ARCHIVE: R2Bucket;
 }
 
 async function do_listing(env: Env, prefix) {
-	const options = {
-		limit: 500,
-		prefix: prefix,
-	};
+  const options = {
+    limit: 500,
+    prefix: prefix,
+  };
 
-	const listed = await env.FOREST_ARCHIVE.list(options);
-	let truncated = listed.truncated;
-	let cursor = listed.truncated ? listed.cursor : undefined;
+  const listed = await env.FOREST_ARCHIVE.list(options);
+  let truncated = listed.truncated;
+  let cursor = listed.truncated ? listed.cursor : undefined;
 
-	// use the truncated property to check if there are more
-	// objects to be returned
-	while (truncated) {
-		const next = await env.FOREST_ARCHIVE.list({
-			...options,
-			cursor: cursor,
-		});
-		listed.objects.push(...next.objects);
+  // use the truncated property to check if there are more
+  // objects to be returned
+  while (truncated) {
+    const next = await env.FOREST_ARCHIVE.list({
+      ...options,
+      cursor: cursor,
+    });
+    listed.objects.push(...next.objects);
 
-		truncated = next.truncated;
-		cursor = next.truncated ? next.cursor : undefined;
-	}
+    truncated = next.truncated;
+    cursor = next.truncated ? next.cursor : undefined;
+  }
 
-	let html = `<!DOCTYPE html>
+  let html = `<!DOCTYPE html>
 		<body>
 		  <h1>Forest Archive</h1>
       <ul>`;
-	for (const obj of listed.objects) {
-		html += `<li><a href="${BASE_URL}/${obj.key}">${obj.key}</a></li>\n`;
-	}
-	html += `</ul></body>`;
+  for (const obj of listed.objects) {
+    html += `<li><a href="${BASE_URL}/${obj.key}">${obj.key}</a></li>\n`;
+  }
+  html += `</ul></body>`;
 
-	return new Response(html, {
-		headers: {
-			'content-type': 'text/html;charset=UTF-8',
-		},
-	});
+  return new Response(html, {
+    headers: {
+      'content-type': 'text/html;charset=UTF-8',
+    },
+  });
 }
 
 export default {
-	async fetch(request: Request, env: Env) {
-		switch (request.method) {
-			case 'GET': {
-				const url = new URL(request.url);
-				const { pathname } = url;
+  async fetch(request: Request, env: Env) {
+    switch (request.method) {
+      case 'GET': {
+        const url = new URL(request.url);
+        const { pathname } = url;
 
-				switch (pathname) {
-					case '/list':
-					case '/list/': {
-						const html = `<!DOCTYPE html>
+        switch (pathname) {
+          case '/list':
+          case '/list/': {
+            const html = `<!DOCTYPE html>
 		      <body>
 		      <h1>Forest Archive</h1>
           <ul>
@@ -65,36 +65,36 @@ export default {
           <li><a href="/list/mainnet/latest">Mainnet Latest</a></li>
           </ul>
           </body>`;
-						return new Response(html, {
-							headers: {
-								'content-type': 'text/html;charset=UTF-8',
-							},
-						});
-					}
-					case '/list/mainnet/diff':
-						return do_listing(env, 'mainnet/diff');
-					case '/list/calibnet/diff':
-						return do_listing(env, 'calibnet/diff');
-					case '/list/mainnet/lite':
-						return do_listing(env, 'mainnet/lite');
-					case '/list/calibnet/lite':
-						return do_listing(env, 'calibnet/lite');
-					case '/list/mainnet/latest':
-						return do_listing(env, 'mainnet/latest');
-					case '/list/calibnet/latest':
-						return do_listing(env, 'calibnet/latest');
-					default:
-						return new Response(`url: ${pathname}`);
-				}
-			}
-			default: {
-				return new Response('Method not allowed', {
-					status: 405,
-					headers: {
-						Allow: 'GET',
-					},
-				});
-			}
-		}
-	},
+            return new Response(html, {
+              headers: {
+                'content-type': 'text/html;charset=UTF-8',
+              },
+            });
+          }
+          case '/list/mainnet/diff':
+            return do_listing(env, 'mainnet/diff');
+          case '/list/calibnet/diff':
+            return do_listing(env, 'calibnet/diff');
+          case '/list/mainnet/lite':
+            return do_listing(env, 'mainnet/lite');
+          case '/list/calibnet/lite':
+            return do_listing(env, 'calibnet/lite');
+          case '/list/mainnet/latest':
+            return do_listing(env, 'mainnet/latest');
+          case '/list/calibnet/latest':
+            return do_listing(env, 'calibnet/latest');
+          default:
+            return new Response(`url: ${pathname}`);
+        }
+      }
+      default: {
+        return new Response('Method not allowed', {
+          status: 405,
+          headers: {
+            Allow: 'GET',
+          },
+        });
+      }
+    }
+  },
 };
