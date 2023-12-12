d8e4a8297c56b5d18ee77a8c795576e074ea4e19
David Himmelstrup <david.himmelstrup@chainsafe.io>
2023-12-12 10:49:38 +0100

    feat: cf worker for listing snapshot files (#259)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Hubert <hubert@chainsafe.io>

---
+++ cf/forest-snapshot-listing/src/worker.ts
@@ -0,0 +1,100 @@
+const BASE_URL = 'https://forest-archive.chainsafe.dev';
+
+interface Env {
+  FOREST_ARCHIVE: R2Bucket;
+}
+
+async function do_listing(env: Env, prefix) {
+  const options = {
+    limit: 500,
+    prefix: prefix,
+  };
+
+  const listed = await env.FOREST_ARCHIVE.list(options);
+  let truncated = listed.truncated;
+  let cursor = listed.truncated ? listed.cursor : undefined;
+
+  // use the truncated property to check if there are more
+  // objects to be returned
+  while (truncated) {
+    const next = await env.FOREST_ARCHIVE.list({
+      ...options,
+      cursor: cursor,
+    });
+    listed.objects.push(...next.objects);
+
+    truncated = next.truncated;
+    cursor = next.truncated ? next.cursor : undefined;
+  }
+
+  let html = `<!DOCTYPE html>
+		<body>
+		  <h1>Forest Archive</h1>
+      <ul>`;
+  for (const obj of listed.objects) {
+    html += `<li><a href="${BASE_URL}/${obj.key}">${obj.key}</a></li>\n`;
+  }
+  html += `</ul></body>`;
+
+  return new Response(html, {
+    headers: {
+      'content-type': 'text/html;charset=UTF-8',
+    },
+  });
+}
+
+export default {
+  async fetch(request: Request, env: Env) {
+    switch (request.method) {
+      case 'GET': {
+        const url = new URL(request.url);
+        const { pathname } = url;
+
+        switch (pathname) {
+          case '/list':
+          case '/list/': {
+            const html = `<!DOCTYPE html>
+		      <body>
+		      <h1>Forest Archive</h1>
+          <ul>
+          <li><a href="/list/calibnet/diff">Calibnet Diffs</a></li>
+          <li><a href="/list/calibnet/lite">Calibnet Lite</a></li>
+          <li><a href="/list/calibnet/latest">Calibnet Latest</a></li>
+          <li><a href="/list/mainnet/diff">Mainnet Diffs</a></li>
+          <li><a href="/list/mainnet/lite">Mainnet Lite</a></li>
+          <li><a href="/list/mainnet/latest">Mainnet Latest</a></li>
+          </ul>
+          </body>`;
+            return new Response(html, {
+              headers: {
+                'content-type': 'text/html;charset=UTF-8',
+              },
+            });
+          }
+          case '/list/mainnet/diff':
+            return do_listing(env, 'mainnet/diff');
+          case '/list/calibnet/diff':
+            return do_listing(env, 'calibnet/diff');
+          case '/list/mainnet/lite':
+            return do_listing(env, 'mainnet/lite');
+          case '/list/calibnet/lite':
+            return do_listing(env, 'calibnet/lite');
+          case '/list/mainnet/latest':
+            return do_listing(env, 'mainnet/latest');
+          case '/list/calibnet/latest':
+            return do_listing(env, 'calibnet/latest');
+          default:
+            return new Response(`url: ${pathname}`);
+        }
+      }
+      default: {
+        return new Response('Method not allowed', {
+          status: 405,
+          headers: {
+            Allow: 'GET',
+          },
+        });
+      }
+    }
+  },
+};
