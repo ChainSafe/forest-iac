b08eefb5cbf8de469b8e8a89a1c4a8ce625b650a
Hubert Bugaj <hubert@chainsafe.io>
2024-01-02 19:55:40 +0100

    remove unused

--- tf-managed/modules/daily-snapshot/monitoring/provider.tf
+++ tf-managed/modules/daily-snapshot/monitoring/provider.tf
@@ -7,10 +7,3 @@ terraform {
     }
   }
 }
-
-# # Configure the New Relic provider
-# provider "newrelic" {
-#   account_id = var.nr_account_id
-#   api_key    = var.nr_api_key
-#   region     = var.nr_region
-# }
--- tf-managed/modules/daily-snapshot/monitoring/variable.tf
+++ tf-managed/modules/daily-snapshot/monitoring/variable.tf
@@ -1,20 +1,3 @@
-# variable "new_relic_account_id" {
-#   type        = string
-#   description = "The New Relic Account ID"
-#   sensitive   = true
-# }
-#
-# variable "new_relic_api_key" {
-#   description = "The New Relic API KEY"
-#   type        = string
-#   sensitive   = true
-# }
-#
-# variable "new_relic_region" {
-#   description = "The New Relic Region"
-#   type        = string
-# }
-
 variable "service_name" {
   description = "The name of the service"
   type        = string
@@ -25,17 +8,3 @@ variable "enable_slack_notifications" {
   type        = bool
   default     = false
 }
-
-# variable "slack_destination_id" {
-#   description = "The unique identifier for the Slack workspace where notifications will be sent."
-#   # TODO: parametrize
-#   default     = "f902e020-5993-4425-9ae3-133084fc870d"
-#   type        = string
-# }
-#
-# variable "slack_channel_id" {
-#   description = "The unique identifier for the Slack channel(forest-notifications), where notifications will be posted."
-#   type        = string
-#   # TODO: parametrize
-#   default     = "C036TCEF0CU"
-# }
