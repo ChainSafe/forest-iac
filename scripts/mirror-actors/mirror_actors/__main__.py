5703465ac49839e07ba3e574e572f4c8ee51e511
samuelarogbonlo <sbayo971@gmail.com>
2024-01-22 14:34:09 +0100

    feat: add linting ci
    Signed-off-by: samuelarogbonlo <sbayo971@gmail.com>

--- scripts/mirror-actors/mirror_actors/__main__.py
+++ scripts/mirror-actors/mirror_actors/__main__.py
@@ -1,7 +1,12 @@
+"""
+This script mirrors Filecoin Actor releases from GitHub to an S3 bucket
+and sends alerts to a Slack channel in case of failures.
+"""
+
 import os
 import re
-import requests
 from datetime import datetime
+import requests
 from dateutil.relativedelta import relativedelta
 import boto3
 from slack_sdk.web import WebClient
@@ -15,7 +20,7 @@ ENDPOINT_URL = os.environ["ENDPOINT_URL"]
 REGION_NAME = os.environ["REGION_NAME"]
 
 GITHUB_REPO = "filecoin-project/builtin-actors"
-release_pattern = r'^v\d+\.\d+\.\d+.*$'
+RELEASE_PATTERN = r'^v\d+\.\d+\.\d+.*$'
 
 # Initialize clients
 slack = WebClient(token=SLACK_API_TOKEN)
@@ -29,6 +34,12 @@ three_years_ago = datetime.now() - relativedelta(years=3)
 
 
 def send_slack_alert(message):
+    """
+    Send an alert message to a predefined Slack channel.
+
+    Args:
+        message (str): The message to be sent to Slack.
+    """
     slack.chat_postMessage(
         channel=SLACK_CHANNEL,
         text=message
@@ -38,26 +49,26 @@ def send_slack_alert(message):
 try:
     releases = github.get_repo(GITHUB_REPO).get_releases()
     # Fetch already mirrored objects from S3
-    s3_response = s3.list_objects(Bucket=BUCKET_NAME)
+    s3_response = s3.list_objects(Bucket=S3_BUCKET)
     already_mirrored = set()
     if 'Contents' in s3_response:
         already_mirrored = set(obj["Key"] for obj in s3_response["Contents"])
 
     for release in releases:
         tag_name = release.tag_name
-        published_at = release.published_at.replace(tzinfo=None)  # Removing timezone info for comparison
-
+        # Removing timezone info for comparison
+        published_at = release.published_at.replace(tzinfo=None)
         # Skip the release if it's older than 3 years
         if published_at < three_years_ago:
             continue
 
-        if re.match(release_pattern, tag_name):
+        if re.match(RELEASE_PATTERN, tag_name):
             for asset in release.get_assets():
                 key = f"{tag_name}/{asset.name}"
                 if key not in already_mirrored:
-                    response = requests.get(asset.browser_download_url)
+                    response = requests.get(asset.browser_download_url, timeout=30)
                     response.raise_for_status()
-                    s3.put_object(Bucket=BUCKET_NAME, Key=key, Body=response.content)
+                    s3.put_object(Bucket=S3_BUCKET, Key=key, Body=response.content)
 
 except Exception as e:
     send_slack_alert(f"⛔ Filecoin Actor mirroring failed: {e}")
