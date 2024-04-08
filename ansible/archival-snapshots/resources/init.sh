9cd8cefabb848c8a9a570d73ce9631e936d99e07
sudo-shashank <shashank@chainsafe.io>
2024-04-08 17:52:08 +0530

    fix logs

--- ansible/archival-snapshots/resources/init.sh
+++ ansible/archival-snapshots/resources/init.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
 
-## Enable strict error handling, command tracing, and pipefail
-set -euxo pipefail
-
-FOREST_CLI="/mnt/md0/exported/archival/forest-v0.17.1/forest-cli"
-FOREST_TOOL="/mnt/md0/exported/archival/forest-v0.17.1/forest-tool"
+# Execute the main.sh script
+./main.sh
 
 CURRENT_SNAPSHOT=$(aws --profile prod --endpoint "$ENDPOINT" s3 ls "s3://forest-archive/mainnet/lite/" | sort | tail -n 1 | awk '{print $NF}')
 CURRENT_EPOCH=$(echo "$CURRENT_SNAPSHOT" | awk -F'_' '{gsub(/[^0-9]/, "", $6); print $6}')
 
-echo "$CURRENT_EPOCH"
-
-LATEST_EPOCH=$($FOREST_CLI sync status | grep "Height:" | awk '{print $2}')
-echo "$LATEST_EPOCH"
-
-while ((LATEST_EPOCH - CURRENT_EPOCH > 30000)); do
-   NEW_EPOCH=$((CURRENT_EPOCH + 30000))
-   NEW_SNAPSHOT=$($FOREST_CLI snapshot export --tipset "$NEW_EPOCH" --depth 30000 | grep forest |awk -F'[:]' '{print $1}')
-
-   # Generate and upload lite snapshot
-   $FOREST_TOOL archive export --epoch "$NEW_EPOCH" --output-path /mnt/md0/exported/archival/lite_snapshots/ "$NEW_SNAPSHOT"
-   aws --profile prod --endpoint "$ENDPOINT" s3 cp /mnt/md0/exported/archival/lite_snapshots/"$NEW_SNAPSHOT" "s3://forest-archive/mainnet/lite/"
-
-   # Generate and upload diff snapshots
-   ./diff_script.sh "$CURRENT_EPOCH" "$CURRENT_SNAPSHOT" "$NEW_SNAPSHOT"
-   ./upload_diff.sh "$ENDPOINT"
-
-   CURRENT_EPOCH=$NEW_EPOCH
-
-   # Send notification on slack channel
-   ruby notify.rb "$CURRENT_EPOCH"
-done
+# Check if the main.sh script executed successfully
+if [ $? -eq 0 ]; then
+    # If successful, call notify.rb with "success"
+    ruby notify.rb "$CURRENT_EPOCH" "success"
+else
+    # If failed, call notify.rb with "failure"
+    ruby notify.rb "$CURRENT_EPOCH" "failure"
+fi
--- ansible/archival-snapshots/resources/notify.rb
+++ ansible/archival-snapshots/resources/notify.rb
@@ -5,9 +5,14 @@ require 'slack-ruby-client'
 CHANNEL = '#forest-notifications'
 SLACK_TOKEN = ENV.fetch('ARCHIVAL_SLACK_TOKEN')
 EPOCH = ARGV[0]
+STATUS = ARGV[1]
 
 client = Slack::Web::Client.new(token: SLACK_TOKEN)
 
-message = "✅ Lite and Diff snapshots updated till #{EPOCH}. 🌲🌳🌲🌳🌲"
+message = if STATUS == 'success'
+            "✅ Lite and Diff snapshots updated till #{EPOCH}. 🌲🌳🌲🌳🌲"
+          else
+            '❌ Failed to update Lite and Diff snapshots. 🔥🌲🔥'
+          end
 
 client.chat_postMessage(channel: CHANNEL, text: message, as_user: true)
